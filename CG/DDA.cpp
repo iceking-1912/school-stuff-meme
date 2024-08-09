1d45b0431d036d87755fabc6b788b83a7fe0f5b9
ARCTICAURA <himanshuhande3.14@gmail.com>
2024-08-09 16:26:24 +0530

    new practicals add assign-2 added

--- CG/DDA.cpp
+++ CG/DDA.cpp
@@ -13,14 +13,37 @@
     N=0
     Cordset = {(X1,Y1),(X1+n(Xinc),Y1+n(Yinc)),
                (X1+(n+1)(Xinc),Y1+(n+1)(Yinc)),(till n+7)(X2,Y2)}
-            (Can also use for or while loop)
+            (Can also use for or whileï¿½loop)
     Stop*/
 
+void DDA(int x1,int y1,int x2,int y2){
+    // int x1=123,x2=850,y1=150,y2=750;
+    int delx = x2 - x1;
+    int dely = y2-y1;
+    int incval = 0;
+    if (delx > dely) {
+        incval = delx;
+    }
+    else (dely > delx)
+                ;{
+        incval = dely;
+    };
+    int xinc = delx / incval;
+    int yinc = dely / incval;
+    xinc = abs(xinc);
+	yinc = abs(yinc);
+    for (int i = 0; i <incval;i++)
+    {
+        putpixel(x1, y1, WHITE);
+        x1 = x1 + xinc;
+        y1 = y1 + yinc;
+    }
+    getch();
+}
 
 int main() {
 
     int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
     
     int x1=123,x2=850,y1=150,y2=750;
     int delx = x2 - x1;
