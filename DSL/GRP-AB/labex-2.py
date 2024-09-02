3cda656f3adcbdc65dd5b3342f7d03ead82444c0
IceKing-1912 <71691466+iceking-1912@users.noreply.github.com>
2024-09-02 19:19:45 +0000

    sub-6-day-1-up

--- DSL/GRP-AB/labex-2.py
+++ DSL/GRP-AB/labex-2.py
@@ -1,6 +1,6 @@
+#labex-2-code
 string = "It is decided that weekly greetings are to be furnished to wish"
-
-print("Given Sttring is", f'"{string}"')
+print("Given String is", f'"{string}"')
 split_string = string.split(" ")
 
 # a) To display word with the longest length
@@ -11,22 +11,31 @@ for i in split_string:
 print("A)The word with the longest length is-", end=" ")
 print(split_string[len_word_spit_string.index(max(len_word_spit_string))])
 
-# b) To determines the frequency of occurrence of particular character in the string
+# b) To determine the frequency of occurrence of each character in the string
 combine_list = ""
 for i in split_string:
     combine_list += i
+
 char_freq = {}
 for char in combine_list:
-    char_freq[combine_list.count(char)] = char
-print("B)The frequency of occurrence of particular character in the string is-", end=" ")
-print(char_freq.get(max(char_freq)))
+    if char in char_freq:
+        char_freq[char] += 1
+    else:
+        char_freq[char] = 1
+
+particular_char = input("Enter the character to find its frequency: ")
+
+if particular_char in char_freq:
+    print(f"B) The frequency of occurrence of '{particular_char}' in the string is {char_freq[particular_char]}")
+else:
+    print(f"B) The character '{particular_char}' is not present in the string")
 
 # c) To check whether given string is palindrome or not
 r_string = string[::-1]
 if string == r_string:
-    print("This String is a Palindrome")
+    print("C) This String is a Palindrome")
 else:
-    print("This is Not a Palindrome")
+    print("C) This is Not a Palindrome")
 
 # d) To display index of first appearance of the substring
 sub_string = "to"
@@ -48,11 +57,12 @@ for i in word_freq:
     print(i, end=" : ")
     print(word_freq.get(i))
 
-# Output
-# Given Sttring is "It is decided that weekly greetings are to be furnished to wish"
+#Output:
+# Given String is "It is decided that weekly greetings are to be furnished to wish"
 # A)The word with the longest length is- greetings
-# B)The frequency of occurrence of particular character in the string is- e
-# This is Not a Palindrome
+# Enter the character to find its frequency: i
+# B) The frequency of occurrence of 'i' in the string is 5
+# C) This is Not a Palindrome
 # D)The Sub-String is- "to"
 # This Sub-String Exist in String at Index- 40
 # e)The occurrences of each word in a given string-
