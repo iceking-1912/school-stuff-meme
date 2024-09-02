4cf99dede4bc783bdf361b59eae19528e2f23393
IceKing-1912 <71691466+iceking-1912@users.noreply.github.com>
2024-09-02 19:19:45 +0000

    sub-6-day-1-up

--- DSL/GRP-AB/labex-4.py
+++ DSL/GRP-AB/labex-4.py
@@ -1,16 +1,94 @@
-def bubble_sort(arr):
-    n = len(arr)
+#labex-4-code
+#bubble_sort
+def bubble_sort(array):
+    temp = [i for i in array]  
+    n = len(temp)
     for i in range(n):
         for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+            if temp[j] > temp[j+1]:
+                temp[j], temp[j+1] = temp[j+1], temp[j]
+    return temp
 
-array = [64, 34, 25, 12, 22, 11, 90]
+#selection_sort
+def selection_sort(array):
+    temp = [i for i in array]  
+    for i in range(len(temp)):
+        min_idx = i
+        for j in range(i+1, len(temp)):
+            if temp[min_idx] > temp[j]:
+                min_idx = j
+        temp[i], temp[min_idx] = temp[min_idx], temp[i]
+    return temp
+#Menu
+f = False
+arr = []
+while (f == False):
+    print("Menu- ")
+    print("1. Add Array")
+    print("2. Selection Sort")
+    print("3. Bubble Sort")
+    print("4. Exit")
+    choice = int(input("Enter your choice (1/2/3/4): "))
+    if choice == 1:
+        n = int(input("No. of Elements in Array:"))
+        arr = []
+        for i in range(n):
+            arr.append(int(input("Enter Element:"))) 
+        print("Array:", arr)
+    elif choice == 2:
+        print("Selection Sort:")
+        print("Unsorted Array:", arr)
+        sorted_array = selection_sort(arr)
+        print("Sorted Array:", sorted_array)
+    elif choice == 3:
+        print("Bubble Sort:")
+        print("Unsorted Array:", arr)
+        sorted_array = bubble_sort(arr)
+        print("Sorted Array:", sorted_array)
+    elif choice == 4:
+        print("Exit")
+        f = True
+    else:
+        print("Invalid choice")
 
-print("Sorted array:", array)
-bubble_sort(array)
-print("Sorted array:", array)
-
-# output:
-# Sorted array: [64, 34, 25, 12, 22, 11, 90]
-# Sorted array: [11, 12, 22, 25, 34, 64, 90]
+# Output:
+# Menu-
+# 1. Add Array
+# 2. Insertion Sort
+# 3. Shell Sort
+# 4. Exit
+# Enter your choice (1/2/3/4):1
+# No. of Elements in Array:7
+# Enter Element:16
+# Enter Element:2
+# Enter Element:69
+# Enter Element:24
+# Enter Element:36
+# Enter Element:24
+# Enter Element:95
+# Array: [16, 2, 69, 24, 36, 24, 95]
+# Menu-
+# 1. Add Array
+# 2. Insertion Sort
+# 3. Shell Sort
+# 4. Exit
+# Enter your choice (1/2/3/4):2
+# Insertion Sort:
+# Unsorted Array: [16, 2, 69, 24, 36, 24,
+# Sorted Array: [2, 16, 24, 24, 36, 69, 95]
+# Menu-
+# 1. Add Array
+# 2. Insertion Sort
+# 3. Shell Sort
+# 4. Exit
+# Enter your choice (1/2/3/4):3
+# Shell Sort:
+# Unsorted Array: [16, 2, 69, 24, 36, 24,
+# 95ted Array: [2, 16, 24, 24, 36, 69, 95]
+# Menu-
+# 1. Add Array
+# 2. Insertion Sort
+# 3. Shell Sort
+# 4. Exit
+# Enter your choice (1/2/3/4):4
+# Exiting ...
--- DSL/GRP-AB/labex-5.py
+++ DSL/GRP-AB/labex-5.py
@@ -1,19 +1,108 @@
+#labex-5-code
 #insertion_sort
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
+def insertion_sort(array):
+    temp = []
+    for i in array:
+        temp.append(i)
+    for i in range(1, len(temp)):
+        key = temp[i]
         j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
+        while j >= 0 and temp[j] > key:
+            temp[j + 1] = temp[j]
             j -= 1
-        arr[j + 1] = key
+        temp[j + 1] = key
+    return temp 
+#shell_sort
+def shell_sort(array):
+    temp = []
+    for i in array:
+        temp.append(i)
+    n = len(temp)
+    gap = n // 2
 
-arr = [5, 2, 8, 12, 1]
-print("UnSorted Array:",arr)
-insertion_sort(arr)
-print("Sorted Array:",arr)
-
+    while gap > 0:
+        for i in range(gap, n):
+            key = temp[i]
+            j = i - gap
+            while j >= 0 and temp[j] > key:
+                temp[j + gap] = temp[j]
+                j -= gap
+            temp[j + gap] = key
+        gap //= 2
+    return temp
+#Menu
+ar = []
+f = False 
+while (f == False):
+    print("Menu- ")
+    print("1. Add Array")
+    print("2. Insertion Sort")
+    print("3. Shell Sort")
+    print("4. Exit")
+    choice = int(input("Enter your choice (1/2/3/4):"))
+    if choice == 1:
+        n = int(input("No. of Elements in Array:"))
+        i = 0
+        arr = []
+        while i<n:
+            arr.append(int(input("Enter Element:"))) 
+            i+=1
+        ar = arr.copy()
+        print("Array:", ar)
+    elif choice == 2:
+        print("Insertion Sort:")
+        print("Unsorted Array:", ar)
+        temparr = insertion_sort(ar)
+        print("Sorted Array:", temparr)
+    elif choice == 3:
+        print("Shell Sort:")
+        print("Unsorted Array:", ar)
+        temparr = shell_sort(ar)
+        print("Sorted Array:", temparr)
+    elif choice == 4:
+        print("Exiting...")
+        f = True
+    else:
+        print("Invalid choice")
+        
 # Output:
-# UnSorted Array: [5, 2, 8, 12, 1]
-# Sorted Array: [1, 2, 5, 8, 12
-
+# Menu-
+# 1. Add Array
+# 2. Insertion Sort
+# 3. Shell Sort
+# 4. Exit
+# Enter your choice (1/2/3/4):1
+# No. of Elements in Array:7
+# Enter Element:35
+# Enter Element:26
+# Enter Element:89
+# Enter Element:22
+# Enter Element:59
+# Enter Element:12
+# Enter Element:34
+# Array: [35, 26, 89, 22, 59, 12, 34]
+# Menu-
+# 1. Add Array
+# 2. Insertion Sort
+# 3. Shell Sort
+# 4. Exit
+# Enter your choice (1/2/3/4):2
+# Insertion Sort:
+# Unsorted Array: [35, 26, 89, 22, 59, 12, 34]
+# Solted Array: [12, 22, 26, 34, 35, 59, 89]
+# Menu-
+# 1. Add Array
+# 2. Insertion Sort
+# 3. Shell Sort
+# 4. Exit
+# Enter your choice (1/2/3/4):3
+# Shell Sort:
+# Unsorted Array: [35, 26, 89, 22, 59, 12, 34]
+# Solted Array: [12, 22, 26, 34, 35, 59, 89]
+# Menu-
+# 1. Add Array
+# 2. Insertion Sort
+# 3. Shell Sort
+# 4. Exit
+# Enter your choice (1/2/3/4):4
+# Exiting ...
